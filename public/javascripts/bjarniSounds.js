39d9fa79a41b2ee6f8e41b0e9fdc138e1ae8ce72
Wolfcoder13 <Wolfninja13@gmail.com>
2017-05-08 23:52:12 +0000

    more clips

--- public/javascripts/bjarniSounds.js
+++ public/javascripts/bjarniSounds.js
@@ -1,6 +1,10 @@
 document.getElementById("bjarni1").addEventListener("click", playBjarni1);
 document.getElementById("bjarni2").addEventListener("click", playBjarni2);
 document.getElementById("bjarni3").addEventListener("click", playBjarni3);
+document.getElementById("bjarni4").addEventListener("click", playBjarni4);
+document.getElementById("bjarni5").addEventListener("click", playBjarni5);
+document.getElementById("bjarni6").addEventListener("click", playBjarni6);
+document.getElementById("bjarni7").addEventListener("click", playBjarni7);
 
 function playBjarni1() {
     var path = "..\\sounds\\"
@@ -17,6 +21,30 @@ function playBjarni2() {
 function playBjarni3() {
     var path = "..\\sounds\\"
     var snd = new Audio(path + "bjarni-UEEAAA.wav");
+    snd.play();
+}
+
+function playBjarni4() {
+    var path = "..\\sounds\\"
+    var snd = new Audio(path + "bjarni-planetEarth.mp3");
+    snd.play();
+}
+
+function playBjarni5() {
+    var path = "..\\sounds\\"
+    var snd = new Audio(path + "bjarni-ohhh.mp3");
+    snd.play();
+}
+
+function playBjarni6() {
+    var path = "..\\sounds\\"
+    var snd = new Audio(path + "bjarni-ehh.mp3");
+    snd.play();
+}
+
+function playBjarni7() {
+    var path = "..\\sounds\\"
+    var snd = new Audio(path + "bjarni - please.mp3");
     snd.play();
 }
 
